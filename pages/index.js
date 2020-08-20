b87db5fb447364b5d8c25230cca9983d8676220e
Seth Snel <sethsnel@gmail.com>
2020-08-20 13:56:21 +0200

    initial update and release

--- pages/index.js
+++ pages/index.js
@@ -75,6 +75,16 @@ class IndexPage extends React.Component {
                     <Head>
                         <title>Next.js Starter</title>
                         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i" rel="stylesheet" />
+                        <script dangerouslySetInnerHTML={{
+                            __html:
+                                `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+                                ga('create', 'UA-28911037-1', 'auto');
+                                ga('send', 'pageview');`
+                        }} />
                     </Head>
 
                     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
